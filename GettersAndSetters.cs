e58dd8969a967cbd5b2c20fa8cf1bce1c8b47133
Cleomir Machado Gomes <cleomir.mg@hotmail.com>
2024-05-11 18:00:40 -0300

    Add getters and setters

---
+++ GettersAndSetters.cs
@@ -0,0 +1,11 @@
+class GettersAndSetters
+{
+    private string name;
+    public string Name {
+        // longer syntax
+        // get { return name; }
+        // set { name = value; }
+
+        // shorter syntax
+        get; set; }
+}
