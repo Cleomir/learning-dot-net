40a7b2a91b4508940626190a7ef51b8d6526af62
Cleomir Machado Gomes <cleomir.mg@hotmail.com>
2024-05-10 22:03:40 -0300

    Add methods

---
+++ Methods.cs
@@ -0,0 +1,15 @@
+namespace Methods
+{
+    public class Methods
+    {
+        public static void Method(string arg1, string arg2, string arg3)
+        {
+            Console.WriteLine($"Arg1: {arg1} Arg2: {arg2} Arg3: {arg3}");
+        }
+
+        public static void Method(int arg1, int arg2, int arg3)
+        {
+            Console.WriteLine($" Arg1: {arg1} Arg2: {arg2} Arg3: {arg3}");
+        }
+    }
+}
